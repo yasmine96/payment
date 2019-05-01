5dd885f41046f574d8b5172a2a9d3087611fb9ce
Yasmine Elsayed <yasminemohamedelsayed@gmail.com>
2019-05-01 08:42:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yasmine/payment/Config/Config.java
@@ -0,0 +1,6 @@
+package com.example.yasmine.payment.Config;
+
+public class Config {
+
+    public static final String PAYPAL_CLIENT_ID = "ASCIli9P4k6jsbkqmx12KFBYydGtZI2uthguQ9lY4pNgUwB_HqwnNG8xZITEWMRJYFF7YWLEr9XN2s9y";
+}
